4905db18b6c3c1dc1bf7000773fd0abe8a25327d
RiotSam <anastasia.pokriskina@gmail.com>
2021-06-19 20:12:23 +0300

    commit 11 into master

--- app/src/main/java/ru/sam/testgit/MainActivity.kt
+++ app/src/main/java/ru/sam/testgit/MainActivity.kt
@@ -1,6 +1,7 @@
 package ru.sam.testgit
 
 import android.os.Bundle
+import android.util.Log
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
@@ -17,6 +18,8 @@ class MainActivity : AppCompatActivity() {
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                     .setAction("Action", null).show()
+
+            Log.d("master", "test 10")
         }
     }
 
